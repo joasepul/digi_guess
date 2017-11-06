0e1e9af715c89e97d99b1111db98f58c7bce7e3a
Unethical <joasepul@ucsc.edu>
2017-11-06 13:45:21 -0800

    added working sgd_clf

--- flask/static/index.js
+++ flask/static/index.js
@@ -1,3 +1,8 @@
+var io = require('socket.io-client');
+var socket = io.connect('http://127.0.0.1:5000');
+socket.on('connect', function() {
+    socket.emit('init', {data: 'I\'m connected!'});
+});
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext("2d");
 
@@ -23,51 +28,60 @@ function redraw() {
     ctx.lineWidth = 5;
 
     clickX.forEach(function(click, i) {
-	    ctx.beginPath();
-	    if (clickDrag[i] && i) {
-		ctx.moveTo(clickX[i-1], clickY[i-1]);
-	    } else {
-		ctx.moveTo(clickX[i] - 1, clickY[i]);
-	    }
-	    ctx.lineTo(clickX[i], clickY[i]);
-	    ctx.closePath();
-	    ctx.stroke();
-	});
+        ctx.beginPath();
+        if (clickDrag[i] && i) {
+            ctx.moveTo(clickX[i-1], clickY[i-1]);
+        } else {
+            ctx.moveTo(clickX[i] - 1, clickY[i]);
+        }
+        ctx.lineTo(clickX[i], clickY[i]);
+        ctx.closePath();
+        ctx.stroke();
+    });
 }
 
 canvas.addEventListener("mousedown", function(e) {
-	var mouseX = e.pageX - this.offsetLeft - this.offsetParent.offsetLeft;
-	var mouseY = e.pageY - this.offsetTop - this.offsetParent.offsetTop;
-	drawing = true;
-	addClick(mouseX, mouseY);
-	redraw();
-    });
+    var mouseX = e.pageX - this.offsetLeft - this.offsetParent.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop - this.offsetParent.offsetTop;
+    drawing = true;
+    addClick(mouseX, mouseY);
+    redraw();
+});
 
 canvas.addEventListener("mousemove", function(e) {
-	if (drawing) {
-	    var mouseX = e.pageX - this.offsetLeft - this.offsetParent.offsetLeft;
-	    var mouseY = e.pageY - this.offsetTop - this.offsetParent.offsetTop;
-	    addClick(mouseX, mouseY);
-	    redraw();
-	}
-    });
+    if (drawing) {
+        var mouseX = e.pageX - this.offsetLeft - this.offsetParent.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop - this.offsetParent.offsetTop;
+        addClick(mouseX, mouseY);
+        redraw();
+    }
+});
 
 canvas.addEventListener("mouseup", function(e) {
-	drawing = false;
-    });
+    drawing = false;
+});
 
 canvas.addEventListener("mouseleave", function(e) {
-	drawing = false;
-    });
+    drawing = false;
+});
 
 document.querySelector('#clear-button').addEventListener("click", function(e) {
-	clearCanvas();
-	clickX = [];
-	clickY = [];
-	clickDrag = [];
-    });
+    clearCanvas();
+    clickX = [];
+    clickY = [];
+    clickDrag = [];
+});
+
+function simplifyArray(imageArr) {
+    var simpleArr = imageArr.filter(
+        (value, index) => (index + 1) % 4 == 0);
+    simpleArr = simpleArr.map(
+        value => value / 255);
+    return Array.from(simpleArr)
+
+}
 
 document.querySelector('#submit-button').addEventListener("click", function(e) {
-	var imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-	console.log(imageData);
-    });
+    var imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    socket.emit('preproc', {data: simplifyArray(imageData.data)});
+});
---
+++ flask/static/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname)
+    },
+    module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
+};
