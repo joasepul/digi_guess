f2f5df7b93ec77709349d500f1e75d8387088885
Harland Duman <dumanjacob@gmail.com>
2017-12-02 15:20:35 -0800

    Reorganized some files and deleted unnecessary ones

--- flask/static/webpack.config.js
+++ flask/static/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 
 module.exports = {
-    entry: './index.js',
+    entry: './js/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname)
