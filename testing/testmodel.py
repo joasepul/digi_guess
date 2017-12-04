548ee3ed49380687a0c68c15b31071b142e24471
Megan <mesharp@ucsc.edu>
2017-12-03 17:59:43 -0800

    changed to fit pep8

--- testing/testmodel.py
+++ testing/testmodel.py
@@ -1,12 +1,12 @@
 import numpy
-import NeuralNetDigitRecon as NN
+import NeuralNetDigitRecon as digrec
 from keras.models import load_model
 
 # import the model
 cnn_model = load_model("../ML_model/cnn_digit_clf.h5")
 
 # test model
-grade = cnn_model.evaluate(NN.X_test, NN.y_test, batch_size=1, verbose=0)
+grade = cnn_model.evaluate(digrec.X_test, digrec.y_test, batch_size=1, verbose=0)
 print("Large CNN Error: %.2f%%" % (100-grade[1]*100))
 
 
