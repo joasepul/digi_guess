c093318c240065e362dd3b86cafa5afdee1d39ff
Unethical <joasepul@ucsc.edu>
2017-12-03 19:26:31 -0800

    added sample preproc data & updated preproc unittest

--- flask/unittests/test_preproc_unittest.py
+++ flask/unittests/test_preproc_unittest.py
@@ -3,7 +3,9 @@ import sys
 sys.path.append('..')
 from preproc import *
 import pickle
+import numpy as np
 
+tempList = []
 class Test_digit_centering(unittest.TestCase):
 
     def setUp(self):
@@ -20,6 +22,16 @@ class Test_digit_centering(unittest.TestCase):
             processed = center_matrix(processed, padding=10)
             display_digit(processed, title="Centered Digit Data")
             self.assertEqual(processed.shape, (100,100))
+            processed = scipy.misc.imresize(processed,
+                                            (28,28),
+                                            interp="lanczos")
+            self.assertEqual(processed.shape, (28,28))
+            display_digit(processed, title="Resized to 28x28")
+            processed = processed.reshape(1, 1, 28, 28).astype('float32')
+            tempList.append(processed)
+        with open("processed_user_data.pickle", "wb") as f:
+            pickle.dump(tempList ,f)
+
 
 
 
