555676aaf130edfdc12995261a6375f2720470c1
Megan <mesharp@ucsc.edu>
2017-12-03 19:33:51 -0800

    trying to get it to work

--- flask/unittests/test_prediction_unittest.py
+++ flask/unittests/test_prediction_unittest.py
@@ -1,26 +1,24 @@
 import unittest
 import sys
 sys.path.append('..')
-from preproc import *
+from model import predict
 import pickle
 
 class Test_digit_centering(unittest.TestCase):
 
     def setUp(self):
-        with open("user_digit_test_input.pickle", "rb") as f:
+        with open("processed_user_data.pickle", "rb") as f:
             input_list = pickle.load(f)
 
-        self.userinput_list = input_list
+        self.preproc_data_list = input_list
         self.label_list = [1,2,3,4,5,6,7,8,9,0]
 
     def test_user_input_processing(self):
 
-        for user_input in self.userinput_list:
-            processed = np.array(user_input).reshape((100, 100))
-            display_digit(processed, title="Uncenterd Digit")
-            processed = center_matrix(processed, padding=10)
-            display_digit(processed, title="Centered Digit")
-            self.assertEqual(processed.shape, (100,100))
+        for index, preproc_data in enumerate(self.preproc_data_list):
+            predicted_label = predict(preproc_data)
+            self.assertEqual(predicted_label, self.label_list[index])
+
 
 
 
